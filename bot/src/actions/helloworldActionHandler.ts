5afb10cb48736992c296f254001c9fb2f672d8bb
Qinen <qinzhu@microsoft.com>
2022-08-04 11:26:10 +0800

    add comments

--- bot/src/actions/helloworldActionHandler.ts
+++ bot/src/actions/helloworldActionHandler.ts
@@ -4,6 +4,10 @@ import { TurnContext } from "botbuilder";
 import helloWorldCard from "../adaptiveCards/helloworldAction.json";
 import { TeamsFxBotActionHandler } from "../sdk/interface";
 
+/**
+ * The `HelloWorldActionHandler` registers an action with the `TeamsFxBotActionHandler` and responds
+ * with an Adaptive Card if the user clicks the Adaptive Card Action with `triggerVerb`.
+ */
 export class HelloWorldActionHandler implements TeamsFxBotActionHandler {
     triggerVerb: string = "acknowledged";
 
